9e41b821acdd1bb9cb0d730c0600dd8526d8f8f8
Vincent Twigt <135724179+vtwigt@users.noreply.github.com>
2024-04-12 16:24:17 +0200

    Add PostCSS with Autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+/**
+ * PostCSS configuration file
+ * https://postcss.org/
+ *
+ * @package RSD_WP
+ * @since 0.8.0
+ * @license Apache-2.0
+ * @link https://research-software-directory.org
+ */
+
+import autoprefixer from 'autoprefixer';
+
+export default {
+	plugins: [
+		autoprefixer
+	]
+}
